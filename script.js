74b8b41dd9c0988bdc0afd56077ffec740dc2af0
Vaishnavikahar <vaishnavikahar33@gmail.com>
2024-09-08 18:10:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('myForm');
+  
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('Form submitted!');
+  // Add your form submission logic here
+});
